a15799197e6c93ccd95ec46f61cc8662bac25dfd
ruhul rabby <ruhulalam1994@gmail.com>
2021-02-09 11:18:05 +0600

    calling data with fetch model

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const searchSongs = () => {
 
 const displaySongs = (songs) => {
     const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
     
     songs.forEach(song => {
         const songDiv = document.createElement('div');
@@ -17,10 +18,26 @@ const displaySongs = (songs) => {
         <div class="col-md-9">
               <h3 class="lyrics-name">${song.title}</h3>
               <p class="author lead">Album by <span>${song.artist.name}</span></p>
+              <audio controls>
+                <source src=" ${song.preview}" type ="audio/mpeg">
+                </audio>
             </div>
             <div class="col-md-3 text-md-right text-center">
-              <button class="btn btn-success">Get Lyrics</button>
-            </div>`;
-            songContainer.appendChild(songDiv);
+              <button onclick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>`;
+        songContainer.appendChild(songDiv);
     });
+}
+
+const getLyric = (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}
+    `
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLyrics())
+}
+
+const displayLyrics = lyrics => {
+    const lytricsDiv = document.getElementById('song-lyrics');
+    lytricsDiv.innerText = lyrics; 
 }
